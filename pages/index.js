4d88282bd550795af612af5221698008e8baa35c
Max <maxabouchar@gmail.com>
2024-02-03 01:36:30 -0800

    Initial content changes

--- pages/index.js
+++ pages/index.js
@@ -6,17 +6,23 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title> My Mindful Diary</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="My Mindful Diary" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          An AI Guided Journaling Experience
         </p>
+        <p>A new line of text</p>
       </main>
-
+      <div className='columns-container'>
+        <div className='column-box'>Encourage Your Journaling Habit</div>
+        <div className='column-box'>Track Your Thoughts and Emotions</div>
+        <div className='column-box'>Be Heard!</div>
+      </div>
+      
       <Footer />
     </div>
   )
