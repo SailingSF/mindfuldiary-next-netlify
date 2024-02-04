ab294ec2fed00caf925cfcb10f5f32c7cffd12b5
Max <maxabouchar@gmail.com>
2024-02-03 19:45:11 -0800

    Add tailwind, improve format

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
@@ -8,20 +9,29 @@ export default function Home() {
       <Head>
         <title> My Mindful Diary</title>
         <link rel="icon" href="/favicon.ico" />
+        <script src='https://cdn.tailwindcss.com'></script>
       </Head>
-
+      <nav className={styles.navbar}>
+        <div className={styles.brand}>Mindful Diary</div>
+        <div className={styles.navLinks}>
+          <a href='#' className={styles.navLinkItem}>About</a>
+          <a href='#' className={styles.navLinkItem}>Features</a>
+          <a href='#' className={styles.navLinkItem}>Team</a>
+        </div>
+      </nav>
       <main>
         <Header title="My Mindful Diary" />
         <p className="description">
           An AI Guided Journaling Experience
         </p>
-        <p>A new line of text</p>
-      </main>
+      <div className='spaceBreak'></div>
+      
       <div className='columns-container'>
         <div className='column-box'>Encourage Your Journaling Habit</div>
         <div className='column-box'>Track Your Thoughts and Emotions</div>
         <div className='column-box'>Be Heard!</div>
       </div>
+      </main>
       
       <Footer />
     </div>
