80c43e92eae9cd1075c9b09a839941d673439764
Max <maxabouchar@gmail.com>
2024-02-04 16:01:37 -0800

    format and add content

--- pages/index.js
+++ pages/index.js
@@ -19,17 +19,42 @@ export default function Home() {
           <a href='#' className={styles.navLinkItem}>Team</a>
         </div>
       </nav>
+      
       <main>
-        <Header title="My Mindful Diary" />
-        <p className="description">
-          An AI Guided Journaling Experience
-        </p>
+      <div style={{ display: 'flex', justifyContent: 'space-between'}}>
+        <div className='intro' style={{ flex: 1}}>
+          <Header title="My Mindful Diary" />
+            <p className="description">
+              An AI Guided Journaling Experience
+            </p>
+        </div>
+        <img src='/mockup_01.png' style={{ width: '33%'}} alt='Mockup Interaction' />
+      </div>
+        
       <div className='spaceBreak'></div>
       
       <div className='columns-container'>
-        <div className='column-box'>Encourage Your Journaling Habit</div>
-        <div className='column-box'>Track Your Thoughts and Emotions</div>
-        <div className='column-box'>Be Heard!</div>
+        <div className='column-box'>
+          <h3>Encourage Your Journaling Habit</h3>
+          <ul>
+            <li>Smart AI Generated Reminders</li>
+            <li>Prompts based on your history</li>
+          </ul>
+        </div>
+        <div className='column-box'>
+          <h3>Track Your Thoughts and Emotions</h3>
+          <ul>
+            <li>Ask for reflections on journal entries</li>
+            <li>Receive analysis and insights</li>
+          </ul>
+        </div>
+        <div className='column-box'>
+          <h3>Be Heard!</h3>
+          <ul>
+            <li>Comforting answers</li>
+            <li>Infinite patience of an AI</li>
+          </ul>
+        </div>
       </div>
       </main>
       
